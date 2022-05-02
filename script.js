a6caa31e4e9a50aecefe3aebf5f78ad16eada03b
labstraction <45163263+labstraction@users.noreply.github.com>
2022-05-02 11:25:26 +0200

    student class

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+
+
+// const student1 = {
+//   name: 'Mirco',
+//   surname: 'Faro',
+//   yob: 1993,
+//   gender: 'm',
+//   gradesArray: grades
+// }
+
+// console.log(student1);
+
+
+// function calculateStudentMean(student){
+
+//   let sum = 0;
+//   for (let i = 0; i < student.gradesArray.length; i++) {
+//     const grade = student.gradesArray[i];
+//     sum = sum + grade;
+//   }
+//   const mean = sum / student.gradesArray.length;
+
+//   return mean;
+// }
+
+// console.log(calculateStudentMean(student1));
+
+const grades = [10, 7, 4, 9, 10];
+
+
+const student2 = new Student('Mirco', 'Faro', 1993, 'm', grades);
+
+
+console.log(student2);
+
+
+console.log(student2, student2.calculateGradeMean());
+
+
+const student3 = new Student("Paolo", "Foppiano", 1997, "m", [10, 9, 2]);
+
+
+console.log(student3.calculateGradeMean());
+
+
+console.log('età mirco', student2.calculateAge());
+console.log("età paolo", student3.calculateAge());
+
+
+console.log(student2.toString());
+console.log(student3.toString());
+
---
+++ student.js
@@ -0,0 +1,43 @@
+class Student {
+
+  constructor(name, surname, yob, gender, gradeArray) {
+    this.name = name;
+    this.surname = surname;
+    this.yob = yob;
+    this.gender = gender;
+    this.gradeArray = gradeArray;
+  }
+
+  calculateGradeMean() {
+    let sum = 0;
+    for (let i = 0; i < this.gradeArray.length; i++) {
+      const grade = this.gradeArray[i];
+      sum = sum + grade;
+    }
+    const mean = sum / this.gradeArray.length;
+
+    return mean;
+  }
+
+  calculateAge(){
+    const now = new Date();
+    const year = now.getFullYear();
+    const age = year - this.yob;
+    return age;
+  }
+
+  
+  toString(){
+    const genere = this.gender === 'f' ? 'femmina' : 'maschio';
+
+    const studentString = 'Nome: ' + this.name + '\n' +
+                          'Cognome: ' + this.surname + '\n' +
+                          'Età: ' + this.calculateAge() + '\n' +
+                          'Genere: ' + genere + '\n' +
+                          'Media: ' + this.calculateGradeMean();
+
+    return studentString;
+  }
+
+
+}
